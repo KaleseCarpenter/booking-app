bb67c03ef0b2c5d19ceb0915e884eb2bd81880dc
Kalese Carpenter <kalesecarpenter@gmail.com>
2022-10-09 09:34:33 -0400

    added user input

--- main.go
+++ main.go
@@ -3,21 +3,34 @@ package main
 import "fmt"
 
 func main() {
-	var conferenceName = "Astro Tech Conference"
+	conferenceName := "Astro Tech Conference"
 	const conferenceTickets = 50 // cannot change value
 	var remainingTickets = 50
 
 	fmt.Printf("conferenceTickets is %T, remainingTickets is %T, conferenceName is %T\n", conferenceTickets, remainingTickets, conferenceName)
 
 	fmt.Printf("Welcome to %v booking application\n", conferenceName)
-	fmt.Printf("We have total of %v tickets and %v are still available", conferenceTickets, remainingTickets)
+	fmt.Printf("We have total of %v tickets and %v are still available\n", conferenceTickets, remainingTickets)
 	fmt.Println("Get your tickets here to attend!")
 
-	var userName string
+	var firstName string
+	var lastName string
+	var email string
 	var userTickets int
 	// ask user for their name
-	userName = "Tom"
+	fmt.Println("Enter your first name:")
+	fmt.Scan(&firstName)
+
+	fmt.Println("Enter your last name:")
+	fmt.Scan(&lastName)
+
+	fmt.Println("Enter your email address:")
+	fmt.Scan(&email)
+
+	fmt.Println("Enter number of tickets:")
+	fmt.Scan(&userTickets)
+
 	userTickets = 2
-	fmt.Printf("User %v booked %v tickets. \n", userName, userTickets)
+	fmt.Printf("Thank you %v %v for booking %v tickets. You will recieve a confirmation email at %v\n", firstName, lastName, userTickets, email)
 
 }
