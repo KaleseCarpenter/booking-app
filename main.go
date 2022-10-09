892ee164ce2e07c9a538f7f035d08b8037016bdb
Kalese Carpenter <kalesecarpenter@gmail.com>
2022-10-09 09:38:53 -0400

    added remaining tickets message

--- main.go
+++ main.go
@@ -5,7 +5,7 @@ import "fmt"
 func main() {
 	conferenceName := "Astro Tech Conference"
 	const conferenceTickets = 50 // cannot change value
-	var remainingTickets = 50
+	var remainingTickets uint = 50
 
 	fmt.Printf("conferenceTickets is %T, remainingTickets is %T, conferenceName is %T\n", conferenceTickets, remainingTickets, conferenceName)
 
@@ -16,7 +16,7 @@ func main() {
 	var firstName string
 	var lastName string
 	var email string
-	var userTickets int
+	var userTickets uint
 	// ask user for their name
 	fmt.Println("Enter your first name:")
 	fmt.Scan(&firstName)
@@ -30,7 +30,9 @@ func main() {
 	fmt.Println("Enter number of tickets:")
 	fmt.Scan(&userTickets)
 
+	remainingTickets = remainingTickets - userTickets
+
 	userTickets = 2
 	fmt.Printf("Thank you %v %v for booking %v tickets. You will recieve a confirmation email at %v\n", firstName, lastName, userTickets, email)
-
+	fmt.Printf("%v tickets remaining for %v\n", remainingTickets, conferenceName)
 }
