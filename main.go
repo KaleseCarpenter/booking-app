618ed3808552701d2666bd6c73a89ac9d3fdc27c
Kalese Carpenter <kalesecarpenter@gmail.com>
2022-10-09 09:09:09 -0400

    beginning setup practice

---
+++ main.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+func main() {
+	var conferenceName = "Astro Tech Conference"
+	const conferenceTickets = 50 // cannot change value
+	var remainingTickets = 50
+
+	fmt.Printf("conferenceTickets is %T, remainingTickets is %T, conferenceName is %T\n", conferenceTickets, remainingTickets, conferenceName)
+
+	fmt.Printf("Welcome to %v booking application\n", conferenceName)
+	fmt.Printf("We have total of %v tickets and %v are still available", conferenceTickets, remainingTickets)
+	fmt.Println("Get your tickets here to attend!")
+
+	var userName string
+	var userTickets int
+	// ask user for their name
+	userName = "Tom"
+	userTickets = 2
+	fmt.Printf("User %v booked %v tickets. \n", userName, userTickets)
+
+}
